bcb1a6317cfc9a59afeb25d3d83c3ca5b897b124
John Nguyen <john.nguyen09@outlook.com>
2020-05-25 23:13:55 +1000

    Turn off notifications

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "${SFTP_KEY}" | base64 --decode > /tmp/beejee.pem
 eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 /tmp/beejee.pem # Allow read access to the private key
 ssh-add /tmp/beejee.pem # Add the private key to SSH
 
+git remote -v
 git config --global push.default matching
 git remote add deploy ssh://git@$SFTP_IP:22$SFTP_DEPLOY_DIR
 git push deploy master
