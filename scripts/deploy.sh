4277e0b6103f9961665beb3a1ad919c79e20ac8c
John Nguyen <john.nguyen09@outlook.com>
2020-05-25 23:22:05 +1000

    Remove depth=1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,6 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 /tmp/beejee.pem # Allow read access to the private key
 ssh-add /tmp/beejee.pem # Add the private key to SSH
 
-git remote -v
 git config --global push.default matching
 git remote add deploy ssh://git@$SFTP_IP:22$SFTP_DEPLOY_DIR
 git push deploy master
