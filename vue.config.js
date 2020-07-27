05066dbb689acb9d81425267a9a7d2ac5657d83b
John Nguyen <john.nguyen09@outlook.com>
2020-07-27 11:48:27 +1000

    Use public folder for supporting favicon.ico

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 module.exports = {
   outputDir: 'build',
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'src/index.html',
-      filename: 'index.html',
-      title: 'BeeJee',
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    }
-  },
   runtimeCompiler: process.env.NODE_ENV === 'development',
 }
